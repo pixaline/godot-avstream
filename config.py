d846d88004f302d759facf9b1b6c5b36afb6b4ef
pixaline <paru@kemono.se>
2026-06-18 17:10:56 +0300

    Android build support

--- config.py
+++ config.py
@@ -5,7 +5,11 @@ def can_build(env, platform):
 
 def configure(env):
     module_path = os.path.dirname(os.path.abspath(__file__))
-    local_ffmpeg = os.path.join(module_path, "ffmpeg", env["platform"] + "_" + env["bits"])
+
+    platform_type = env["bits"]
+    if (env["platform"] == "android"):
+        platform_type = env["android_arch"]
+    local_ffmpeg = os.path.join(module_path, "ffmpeg", env["platform"] + "_" + platform_type)
 
     if os.path.isdir(local_ffmpeg):
         print("av_stream: using local FFmpeg headers from " + local_ffmpeg)
